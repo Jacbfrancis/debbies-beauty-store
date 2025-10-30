e0992374a04e0617a0fff7abf0b59617b4b54dfa
Francis Jacob <dev.francisjacob@gmail.com>
2025-10-30 11:38:42 +0100

    implemented local storage for cart items

--- src/pages/ProductDetailsPage.jsx
+++ src/pages/ProductDetailsPage.jsx
@@ -1,11 +1,13 @@
 import ProductDetails from "../components/ProductDetails";
 import SuggestProducts from "../components/SuggestProducts";
+import CustomerAssurance from "../components/CustomerAssurance";
 
 function ProductDetailsPage() {
   return (
     <>
       <ProductDetails />
       <SuggestProducts />
+      <CustomerAssurance />
     </>
   );
 }
--- src/store/useCart.jsx
+++ src/store/useCart.jsx
@@ -1,11 +1,18 @@
 import { create } from "zustand";
+import { persist } from "zustand/middleware";
 
-export const useCart = create((set) => ({
-  cart: [],
-  addToCart: (product) => set((state) => ({ cart: [...state.cart, product] })),
-  removeFromCart: (productID) =>
-    set((state) => ({
-      ...state,
-      cart: state.cart.filter((cart) => cart.id !== productID),
-    })),
-}));
+export const useCart = create(
+  persist(
+    (set) => ({
+      cart: [],
+      addToCart: (product) =>
+        set((state) => ({ cart: [...state.cart, product] })),
+      removeFromCart: (productID) =>
+        set((state) => ({
+          ...state,
+          cart: state.cart.filter((cart) => cart.id !== productID),
+        })),
+    }),
+    { name: "cart-storage" }
+  )
+);
