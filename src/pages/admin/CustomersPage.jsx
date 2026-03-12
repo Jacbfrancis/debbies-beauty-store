817e23dab6803c57afd9652c2fa50da7c9902129
Francis Jacob <dev.francisjacob@gmail.com>
2026-03-12 14:33:09 +0100

    fixed bug on customers page

--- src/pages/admin/CustomersPage.jsx
+++ src/pages/admin/CustomersPage.jsx
@@ -20,9 +20,9 @@ export default function CustomersPage() {
     return getUsers;
   }, []);
 
-  if (users.length === 0) {
-    return <LoadingPage />;
-  }
+  // if (users.length === 0) {
+  //   return <LoadingPage />;
+  // }
 
   return (
     <div className="px-6 md:w-[75%]">
